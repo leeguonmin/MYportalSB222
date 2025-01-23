54693d043ccc02c8fc86c6074f7d5e0a837b411f
lee <l_lpp@naver.com>
2025-01-23 14:36:39 +0900

    이건 선생님이랑 하는거 (222 안붙어있는 오리지널은 혼자 해보는걸로) boardMapper 하는거야? 왜 추가된겨

---
+++ src/main/java/himedia/myportal/mappers/BoardMapper.java
@@ -0,0 +1,25 @@
+package himedia.myportal.mappers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+//mappers/board.xml에 정의된 쿼리 객체와 메서드 1:1 매핑
+@Mapper
+public interface BoardMapper {
+//<select id="selectAll"
+//		resultType="boardVo">
+List<BoardVo> selectAll();
+//<insert id="insert" parameterType="boardVo">
+int insert(BoardVo vo);
+//<update id="increaseHitCount" parameterType="int">
+int increaseHitCount(int id);
+//<select id="getContent" parameterType="int"
+//		resultType="boardVo">
+BoardVo getContent(Integer id);
+//<update id="update" parameterType="boardVo">
+int update(BoardVo vo);
+//<delete id="delete" parameterType="map">
+int delete(Map<String, Integer> map);
+}

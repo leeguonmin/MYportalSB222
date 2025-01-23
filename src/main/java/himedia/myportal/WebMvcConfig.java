3c1633c263363903c6321ce221a0a742ed0a35d2
lee <l_lpp@naver.com>
2025-01-23 16:21:41 +0900

    추가

--- src/main/java/himedia/myportal/WebMvcConfig.java
+++ src/main/java/himedia/myportal/WebMvcConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import himedia.myportal.interceptors.AuthInterceptor;
+import himedia.myportal.interceptors.LogoutInterceptor;
+
 // mvc 설정 클래스 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -12,9 +15,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	// 인터셉터 설정
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		// TODO Auto-generated method stub
-		WebMvcConfigurer.super.addInterceptors(registry);
+		registry.addInterceptor(new LogoutInterceptor()).addPathPatterns("/users/logout");
+		
+		// AuthInterceptor
+		// /board/ 하위로 모든 URL은 막아두고(인증 걸어두고)
+		// 목록된 인증을 거치지 않는다
+		registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/board/**")		// board 하위 경로에 인터셉트 적용 
+		.excludePathPatterns("/board", "/board/", "/board/list");
+		
 	}
+	
+	
 	
 	
 	

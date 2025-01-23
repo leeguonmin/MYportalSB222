0ace402f4e2d4dd2d82b6d19d5f50cc5a9a0ad52
lee <l_lpp@naver.com>
2025-01-23 14:55:55 +0900

    xml이랑 mapper랑 1:1연결

--- src/main/java/himedia/myportal/mappers/UserMapper.java
+++ src/main/java/himedia/myportal/mappers/UserMapper.java
@@ -1,6 +1,16 @@
 package himedia.myportal.mappers;
 
+import himedia.myportal.repositories.vo.UserVo;
+
 public interface UserMapper {
 	
-
+// <insert id="insert" parameterType="userVo">
+	int insert(UserVo vo);
+	
+//	<select id="selectUserByEmailAndPassword" parameterType="map" resultType="userVo">
+	
+	
+//	<select id="selectUserByEmail" parameterType="string" resultType="userVo">
+	
+	
 }
